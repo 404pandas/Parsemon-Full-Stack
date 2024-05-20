8a4ef08a34f9955b7df11b3867a51be0000ccaa8
404pandas <113853550+404pandas@users.noreply.github.com>
2024-05-20 16:17:40 -0400

    I quiiiiiittttt

--- controllers/api/deckBuilderRoutes.js
+++ controllers/api/deckBuilderRoutes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router()
 const { Deck } = require('../../models/')
 const { apiGuard } = require('../../utils/authGuard')
+
+// Creating a new deck
+router.post('/', apiGuard, async (req, res) => {
+  const { name, cards, totalCards } = req.body
+
+  try {
+    const newDeck = await Deck.create({
+      name: name,
+      cards: cards,
+      totalCards: totalCards,
+      userId: req.session.user_id,
+    })
+    res.json(newDeck)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 // Updating deck - drag in drag out on right hand side
 router.put('/:id', apiGuard, async (req, res) => {
   try {
--- public/js/deckBuilder.js
+++ public/js/deckBuilder.js
@@ -1,16 +1,116 @@
-function updateToggleButtons() {
-  console.log('OIKJKFJLAEJAIFWJI')
-  leftColumn.find('.card').each(function () {
-    const toggleBtn = $(this).find('.toggle-btn')
-    toggleBtn.text('+')
-  })
+// Update a deck deck
+$(document).ready(function () {
+  $('#save-deck-btn').click(function () {
+    const deckName = $('#deck-name-input').val() // Move this line here
+    const deckId = $('#deck-carousel .carousel-item.active').data('deck-id')
+    const cards = []
 
-  rightColumn.find('.card').each(function () {
-    const toggleBtn = $(this).find('.toggle-btn')
-    toggleBtn.text('-')
-  })
-}
+    // Object to store card names and their quantities
+    const cardQuantities = {}
 
+    // Loop through each card in the right column
+    $('#right-column .pokemon-card').each(function () {
+      const cardName = $(this).find('.card-title')[0].firstChild.textContent.trim()
+
+      // Increment the quantity of the card name in the object
+      if (cardQuantities.hasOwnProperty(cardName)) {
+        cardQuantities[cardName]++
+      } else {
+        cardQuantities[cardName] = 1
+      }
+    })
+
+    // Calculate total cards
+    let totalCards = 0
+    for (const cardName in cardQuantities) {
+      if (cardQuantities.hasOwnProperty(cardName)) {
+        totalCards += cardQuantities[cardName]
+      }
+    }
+
+    // Convert cardQuantities object to an array of cards
+    Object.entries(cardQuantities).forEach(([name, quantity]) => {
+      cards.push({ name, quantity })
+    })
+
+    fetch(`/api/deckBuilder/${deckId}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: deckName,
+        cards: cards,
+        totalCards: totalCards, // Add totalCards to the request body
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  })
+})
+
+// Create a new deck
+$(document).ready(function () {
+  $('#create-deck-btn').click(function () {
+    const deckName = $('#deck-name-input').val() // Move this line here
+
+    const cards = []
+
+    // Object to store card names and their quantities
+    const cardQuantities = {}
+
+    // Loop through each card in the right column
+    $('#right-column .pokemon-card').each(function () {
+      const cardName = $(this).find('.card-title')[0].firstChild.textContent.trim()
+
+      // Increment the quantity of the card name in the object
+      if (cardQuantities.hasOwnProperty(cardName)) {
+        cardQuantities[cardName]++
+      } else {
+        cardQuantities[cardName] = 1
+      }
+    })
+
+    // Calculate total cards
+    let totalCards = 0
+    for (const cardName in cardQuantities) {
+      if (cardQuantities.hasOwnProperty(cardName)) {
+        totalCards += cardQuantities[cardName]
+      }
+    }
+    // Convert cardQuantities object to an array of cards
+    Object.entries(cardQuantities).forEach(([name, quantity]) => {
+      cards.push({ name, quantity })
+    })
+    fetch(`/api/deckBuilder/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: deckName,
+        cards: cards,
+        totalCards: totalCards,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          console.error('Error:', error)
+          // Display error button
+          $('#error-button').show()
+          // Attach click event to error button
+          $('#error-button').click(() => {
+            $('#error-details').text(error.message).toggle()
+          })
+        }
+      })
+      .then(() => {
+        location.reload()
+      })
+  })
+})
+
+// Load deck
 $(document).ready(function () {
   const leftColumn = $('#left-column')
   const rightColumn = $('#right-column')
@@ -19,6 +119,14 @@ $(document).ready(function () {
   leftColumn.find('.pokemon-card').draggable({
     revert: 'invalid',
     helper: 'clone',
+    drag: function (event, ui) {
+      const count = $('#right-column').find('.pokemon-card').length
+      const span = $('#card-count')
+      console.log(count)
+      span.text(count)
+      // Return the count
+      return count
+    },
   })
 
   // Initialize Droppable for the right column
@@ -30,8 +138,14 @@ $(document).ready(function () {
 
       $(this).append(clonedCard)
 
+      clonedCard.find('.toggle-btn').text('-').addClass('remove-card')
       // Update toggle buttons after dropping a card
-      updateToggleButtons()
+      const count = $('#right-column').find('.pokemon-card').length
+      const span = $('#card-count')
+      console.log(count)
+      span.text(count)
+      // Return the count
+      return count
     },
   })
   // Add listeners for drag events if needed
@@ -41,10 +155,24 @@ $(document).ready(function () {
     const toggleBtn = draggedCard.find('.toggle-btn')
     toggleBtn.text('-')
     console.log('Drag started')
+    const count = $('#right-column').find('.pokemon-card').length
+    const span = $('#card-count')
+    console.log(count)
+    span.text(count)
+    // Return the count
+    return count
   })
 
   leftColumn.on('dragstop', '.pokemon-card', () => {
     console.log('Drag stopped')
+  })
+  rightColumn.on('drop', function (event, ui) {
+    const count = $('#right-column').find('.pokemon-card').length
+    const span = $('#card-count')
+    console.log(count)
+    span.text(count)
+    // Return the count
+    return count
   })
 
   function updateColumnStyles() {
@@ -57,7 +185,7 @@ $(document).ready(function () {
     })
   }
 
-  function updateToggleButtons() {
+  function countPokemonCards() {
     $('.toggle-btn').each(function () {
       const card = $(this).closest('.card')
       const isInRightColumn = card.parent().is(rightColumn)
@@ -71,7 +199,7 @@ $(document).ready(function () {
   }
 
   updateColumnStyles()
-  updateToggleButtons()
+  countPokemonCards()
 
   // Toggle card content visibility on icon click
   $('.more-btn').click(function () {
@@ -86,20 +214,41 @@ $(document).ready(function () {
 
   // Toggle adding/removing from the right column
   $(document).on('click', '.toggle-btn', function () {
+    const rightColumn = $('#right-column')
     const card = $(this).closest('.card')
-    const isInRightColumn = card.parent().is(rightColumn)
 
-    if (isInRightColumn) {
-      // Remove card from the right column
-      card.remove()
-    } else {
-      // Add card to the right column
-      rightColumn.append(
-        card.clone().addClass('col s5 pokemon-card card ui-draggable ui-draggable-handle'),
-      )
-      updateToggleButtons()
-    }
+    // Clone the card and add necessary classes
+    const clonedCard = card
+      .clone()
+      .addClass('col s5 pokemon-card card ui-draggable ui-draggable-handle')
 
-    updateColumnStyles()
+    // Update toggle button and append cloned card to the right column
+    clonedCard.find('.toggle-btn').text('-').addClass('remove-card')
+    rightColumn.append(clonedCard)
+
+    // Update card count
+    const count = rightColumn.find('.pokemon-card').length
+    const span = $('#card-count')
+    span.text(count)
+
+    // Return the count (optional)
+    return count
   })
+})
+
+$(document).on('click', '.toggle-btn.remove-card', function () {
+  const selectedCard = $(this).parent('.pokemon-card')
+  // Ensure selectedCard exists
+  if (selectedCard.length > 0) {
+    selectedCard.remove()
+    const count = $('#right-column').find('.pokemon-card').length
+    const span = $('#card-count')
+    console.log('Card removed:', selectedCard)
+    span.text(count)
+  } else {
+    console.log('Unable to find parent card element.')
+  }
+})
+$(document).ready(function () {
+  $('.carousel').carousel()
 })
